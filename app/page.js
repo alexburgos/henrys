d03cda0a90a7f1890924fb19bf65c1d8de8fc256
Alex Burgos <alexander.burgos@kiwi.com>
2024-07-01 17:41:04 +0300

    adjusting position of diamond and logo images, color changes

--- app/page.js
+++ app/page.js
@@ -13,31 +13,31 @@ export default function Home() {
         alt='Diamond pattern'
         width="auto"
         height="auto"
-        className='mb-24 mt-16 absolute top-[-15rem] sm:top-[-20rem] md:top-[-30rem] z-0'
+        className='absolute top-[-8rem] sm:top-[-18rem] md:top-[-30rem] lg:top-[-26rem] z-0'
       />
       <Image
         src={HenrysLogo}
         alt='Henrys Logo'
         width={500}
         height={500}
-        className='mt-12 mb-12 md:mt-20 md:mb-24'
+        className='mt-12 mb-12 md:mt-20 lg:mb-24'
       />
       <div className='flex flex-col sm:w-full md:w-auto sm:flex-row space-y-6 sm:space-y-0 sm:space-x-10 text-center'>
         <Link
           href='/food'
-          className='w-56 border-2 border-henrys text-henrys-beige bg-henrys cursor-pointer hover:bg-henrys-beige hover:border-henrys hover:text-henrys'
+          className='w-52 border-2 border-henrys text-henrys-beige bg-henrys cursor-pointer hover:bg-henrys-beige hover:border-henrys hover:text-henrys'
         >
           Food
         </Link>
         <Link
           href='/drinks'
-          className='w-56 border-2 border-henrys text-henrys-beige bg-henrys cursor-pointer hover:bg-henrys-beige hover:border-henrys hover:text-henrys'
+          className='w-52 border-2 border-henrys text-henrys-beige bg-henrys cursor-pointer hover:bg-henrys-beige hover:border-henrys hover:text-henrys'
         >
           Drinks
         </Link>
         <Link
           href='/about'
-          className='w-56 border-2 border-henrys text-henrys-beige bg-henrys cursor-pointer hover:bg-henrys-beige hover:border-henrys hover:text-henrys'
+          className='w-52 border-2 border-henrys text-henrys-beige bg-henrys cursor-pointer hover:bg-henrys-beige hover:border-henrys hover:text-henrys'
         >
           About us
         </Link>
